8be8f7615c9000ea75046d37091676d6018705f0
Ovidiu Bodea <antarexis@gmail.com>
2016-08-20 19:17:16 +0300

    Website page displays data

--- client/helpers.js
+++ client/helpers.js
@@ -11,14 +11,33 @@ Template.website_list.helpers({
 Template.website_list.onCreated(function() {
   this.autorun(() => {
     this.subscribe('websites');
-    this.subscribe('voters');
   });
 });
 
 ////////
-// Website_item helpers
+// Website_show
 ////////
+Template.Website_show_page.onCreated(function() {
+  this.getWebsiteId = () => FlowRouter.getParam('_id');
 
-Template.website_item.helpers({
+  this.autorun(() => {
+    this.subscribe('website', this.getWebsiteId());
+  });
+});
 
-})
+Template.Website_show_page.helpers({
+    websiteReady: ()=> {
+      const instance = Template.instance();
+      return instance.subscriptionsReady()
+    },
+    // We pass `list` (which contains the full list, with all fields, as a function
+    // because we want to control reactivity. When you check a todo item, the
+    // `list.incompleteCount` changes. If we didn't do this the entire list would
+    // re-render whenever you checked an item. By isolating the reactiviy on the list
+    // to the area that cares about it, we stop it from happening.
+    website() {
+      const instance = Template.instance();
+      const listId = instance.getWebsiteId();
+      return Websites.findOne({_id: listId});
+    }
+});
--- server/pubs.js
+++ server/pubs.js
@@ -4,6 +4,10 @@ Meteor.publish("voters", function() {
 Meteor.publish("websites", function() {
 	return Websites.find();
 });
+Meteor.publish("website", (websiteId)=> {
+	const pub = Websites.find({_id: websiteId});
+	return pub;
+});
 Meteor.publish("allUserData", function () {
   return Meteor.users.find({}, {fields: {'profile': 1}});
 });
